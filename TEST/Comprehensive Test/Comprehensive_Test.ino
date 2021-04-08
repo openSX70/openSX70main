d393b59e986f48f644458230f5efeb88037314c2
zanepollard <pollardzane@gmail.com>
2021-04-08 10:22:06 -0500

    Beginning of comprehensive test software

---
+++ TEST/Comprehensive Test/Comprehensive_Test.ino
@@ -0,0 +1,8 @@
+#include "Arduino.h"
+#include "open_sx70.h"
+
+/*
+    TODO gotta think how I want to do this. In camera? With an external program to control the test being performed? 
+    I'm thinking the latter would be best. That way we can output diagnostics to a text file or something. 
+    This would allow users with issues to get more directed help and feedback based on the test result they were given.
+*/
